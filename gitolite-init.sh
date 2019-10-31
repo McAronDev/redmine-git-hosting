bb82af8334df75bf5fe9b5165987ccc2b4bf9c15
Roman Nadtochii <romanadt@gmail.com>
2019-10-31 06:39:44 +0100

    check if gitolite initialized

--- gitolite-init.sh
+++ gitolite-init.sh
@@ -8,12 +8,16 @@ if [ ! -d "/ssh_keys" ]; then
     echo -e 'Defaults:redmine !requiretty\nredmine ALL=(git) NOPASSWD:ALL' > /etc/sudoers.d/redmine
     chmod 440 /etc/sudoers.d/redmine
     
-    su git -c "gitolite setup -pk /ssh_keys/redmine_gitolite_admin_id_rsa.pub"
+
     su redmine -s /bin/sh -c "mkdir /home/redmine/.ssh"
     su redmine -s /bin/sh -c "ssh-keyscan -H 127.0.0.1 > /home/redmine/.ssh/known_hosts"
-    passwd -u git # unlocking user to allow ssh access
-    sed -i "s|#[[:space:]]*LOCAL_CODE.*\"\$ENV{HOME}/local\"|LOCAL_CODE\ =>\ \"\$ENV{HOME}/local\"|" /var/lib/git/.gitolite.rc
-    sed -i "s|GIT_CONFIG_KEYS.*|GIT_CONFIG_KEYS\ =>\ \'\.\*\',|"  /var/lib/git/.gitolite.rc
+
+    if [ ! -f "/var/lib/git/.gitolite.rc" ]; then
+        su git -c "gitolite setup -pk /ssh_keys/redmine_gitolite_admin_id_rsa.pub"
+        passwd -u git # unlocking user to allow ssh access
+        sed -i "s|#[[:space:]]*LOCAL_CODE.*\"\$ENV{HOME}/local\"|LOCAL_CODE\ =>\ \"\$ENV{HOME}/local\"|" /var/lib/git/.gitolite.rc
+        sed -i "s|GIT_CONFIG_KEYS.*|GIT_CONFIG_KEYS\ =>\ \'\.\*\',|"  /var/lib/git/.gitolite.rc
+    fi
     
 fi
 
